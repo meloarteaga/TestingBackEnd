659b7b1d72ca7ca76db3fb8f5a87fb2711576cc7
meloarteaga <73600799+meloarteaga@users.noreply.github.com>
2021-12-08 12:23:41 -0600

    made a python package

--- app/run_example.py
+++
@@ -1,7 +0,0 @@
-from app.summary import Summarize
-import io
-
-text = io.open(
-    "/Users/charleszhang/Desktop/MiMIC/docs/text0.txt", "r", encoding="utf-8"
-).read()
-print(Summarize(text, lang="english").summarize(length=5))
